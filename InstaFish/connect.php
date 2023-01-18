a1236a11adca1d213892871da167f5ec76cd2175
kay-server-iskoa-music <iskoa@kay-software.ru>
2023-01-18 20:29:46 +0500

    2

--- InstaFish/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connect  = mysqli_connect('localhost', 'USERNAME', 'PASSWORD', 'DATABASE_NAME')
-
-?>
--- InstaFish/create.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'connect.php';
-
-$login = $_POST['login'];
-$password = $_POST['password'];
-
-mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL, '$login', '$password')");
-?>
